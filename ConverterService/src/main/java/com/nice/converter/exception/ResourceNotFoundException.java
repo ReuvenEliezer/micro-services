260b6121e25128047e19403dd8b4c7eb9bb4242b
Eliezer <reuvene@mprest.com>
2021-07-01 20:12:07 +0300

    init

---
+++ ConverterService/src/main/java/com/nice/converter/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.nice.exception;
+
+
+/**
+ * Create Custom Exception
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
---
+++ ConverterService/src/main/java/com/nice/converter/services/ConverterServiceImpl.java
@@ -0,0 +1,76 @@
+package com.nice.services;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class ConverterServiceImpl implements ConverterService {
+
+    private final static Logger logger = LogManager.getLogger(ConverterServiceImpl.class);
+
+    @Autowired
+    private QueueService queueService;
+
+    @Override
+    public BigDecimal convertStr(String str) {
+        validateStr(str);
+        int result = 0;
+        for (int value : str.toCharArray()) {
+            result += value;
+        }
+        BigDecimal bigDecimal = new BigDecimal(result);
+        queueService.put(bigDecimal);
+        return bigDecimal;
+    }
+
+    @Override
+    public BigDecimal convertHex(String hex) {
+        validateHex(hex);
+        long value = Long.parseLong(hex, 16);
+        BigDecimal bigDecimal = new BigDecimal(value);
+        queueService.put(bigDecimal);
+        return bigDecimal;
+    }
+
+    @Override
+    public BigDecimal convertFraction(String fraction) {
+        validateFraction(fraction);
+        String[] split = fraction.split("/");
+        BigDecimal value = BigDecimal.valueOf(Long.valueOf(split[0]).longValue()).divide(BigDecimal.valueOf(Long.valueOf(split[1]).longValue()));
+        queueService.put(value);
+        return value;
+    }
+
+    private void validateHex(String hex) {
+        Pattern p = Pattern.compile("[0-9a-fA-F]+");
+        Matcher m = p.matcher(hex);
+        if (!m.matches()) {
+            String message = String.format("hex %s not valid", hex);
+            logger.error(message);
+            throw new IllegalArgumentException(message);
+
+        }
+    }
+
+    private void validateStr(String str) {
+        if (!str.chars().allMatch(Character::isLetter)) {
+            String message = String.format("str %s contains a numeric value", str);
+            logger.error(message);
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void validateFraction(String fraction) {
+        if (!fraction.contains("/") || fraction.split("/").length != 2) {
+            String message = String.format("fraction %s not valid. expected '/' between 2 numbers", fraction);
+            logger.error(message);
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
