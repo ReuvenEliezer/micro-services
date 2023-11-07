14d58deefbe5ce74e2bed511f715e74aaf78f849
eliezerr <eliezer.reuven@nice.com>
2023-11-07 19:39:05 +0200

    spring boot admin

---
+++ admin/src/main/java/com/nice/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.nice.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication {
+    //http://localhost:8080/applications
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
