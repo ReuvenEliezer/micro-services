06d26ca2a8edd145a7d235e7d04abef241660b93
eliezerr <eliezer.reuven@nice.com>
2024-02-25 22:02:22 +0200

    - add integration test by docker-compose.yml - fix zipkin service

--- admin/src/test/java/com/nice/admin/AdminApplicationTests.java
+++ admin/src/test/java/com/nice/admin/AdminApplicationTests.java
@@ -12,26 +12,26 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = AdminApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AdminApplication.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AdminApplicationTests {
     private static final String localhost = "http://localhost:";
 
     private static final RestTemplate restTemplate = new RestTemplate();
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @AfterAll
-    public void tearDown(){
-        SpringApplication.exit(applicationContext);
-    }
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    @AfterAll
+//    public void tearDown(){
+//        SpringApplication.exit(applicationContext);
+//    }
 
     @Test
-    @Disabled
     void healthByZipkinTest() {
         String res = restTemplate.getForObject(localhost + "9411/zipkin", String.class);
         assertThat(res).isNotNull();
     }
+
 }
