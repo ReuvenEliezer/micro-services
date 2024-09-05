a0d6bd5b76ed1cebb40148f06b4c7558a8ef5db1
eliezerr <eliezer.reuven@nice.com>
2024-09-05 20:28:37 +0300

    support docker-compose

--- admin/src/main/java/com/nice/admin/AdminApplication.java
+++ admin/src/main/java/com/nice/admin/AdminApplication.java
@@ -3,11 +3,14 @@ package com.nice.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AdminApplication {
 
+    //https://docs.spring-boot-admin.com/current/getting-started.html
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
     }
